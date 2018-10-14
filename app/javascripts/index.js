0034551bffb95f20471daba8c7018025670039b6
dzucconi <mail@damonzucconi.com>
2018-10-14 12:20:10 -0400

    Use CamelCase

--- app/javascripts/index.js
+++ app/javascripts/index.js
@@ -2,11 +2,11 @@ import parameters from 'queryparams';
 
 window.parameters = parameters;
 
-const DOM = {
-  app: document.getElementById('app'),
-};
-
 export default () => {
+  const DOM = {
+    app: document.getElementById('App'),
+  };
+
   const { message } = parameters({ message: 'Hello' });
 
   DOM.app.innerHTML = message;
